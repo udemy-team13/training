7b7c10c4aa1ea0d156ce854e405109773b43af45
Yuni0221 <moonrise002002@gmail.com>
2023-06-21 09:31:28 +0900

    Todo list 기능 수정

--- Yuni/my-todolist/my-todolist.js
+++ Yuni/my-todolist/my-todolist.js
@@ -35,8 +35,11 @@ function greeting(event) {
   event.preventDefault();
 
   let name = document.querySelector(".input_name").value;
-  let greeting = document.createElement("div");
-  greeting.innerHTML = `Hello, ${name} !`;
 
+  const removeInput = event.target;
+  removeInput.remove();
+
+  let greeting = document.createElement("h2");
+  greeting.innerHTML = `Hello, ${name}!`;
   document.querySelector(".greeting").append(greeting);
 }
