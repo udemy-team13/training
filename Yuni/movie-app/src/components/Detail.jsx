8620a68cc49e8e20d05f6a6b6e13cb5b10742769
Yuni0221 <moonrise002002@gmail.com>
2023-06-27 14:31:15 +0900

    Loading CSS 추가 수정 🤓

--- Yuni/movie-app/src/components/Detail.jsx
+++ Yuni/movie-app/src/components/Detail.jsx
@@ -1,26 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styles from "./Detail.module.css";
+import Loading from "./Loading";
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
       .then((response) => response.json())
       .then((json) => {
+        setLoading(false);
         setMovies(json.data.movie);
       });
   }, []);
   console.log(movies);
   return (
     <>
-      <div>
-        <img src={movies.large_cover_image} alt="imgDetail" />
-      </div>
-      <div>{movies.title}</div>
-      <div>{movies.description_full}</div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className={styles.wrapper}>
+          <img
+            src={movies.large_cover_image}
+            alt="imgDetail"
+            className={styles.img}
+          />
+          <div>
+            <div className={styles.title}>{movies.title}</div>
+            <div className={styles.rating}>⭐️ {movies.rating}</div>
+            <div className={styles.info}>{movies.description_full}</div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
