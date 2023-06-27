5c06bcc0181026880d9953c5ddfe8747470363ec
Yuni0221 <moonrise002002@gmail.com>
2023-06-27 13:47:18 +0900

    Loading 기능 추가

---
+++ Yuni/movie-app/src/components/Loading.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./Loading.module.css";
+import { Hearts } from "react-loader-spinner";
+
+function Loading() {
+  return (
+    <div className={styles.loading}>
+      <Hearts
+        className={styles.hearts}
+        height="100"
+        width="100"
+        color="Coral"
+        ariaLabel="hearts-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  );
+}
+export default Loading;
